e03b6311fd110cb2da17600296c4cdd4bf273b05
DracoNocte <dragonnight.gaming@gmail.com>
2023-11-29 01:24:44 +0100

    build: minimize imports scope

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export * from '@models';
+/* MODELS */
+// Common
+export * from '@models/common';
+// Roleplay
+export * from '@models/roleplay/asset';
+export * from '@models/roleplay/character';
+export * from '@models/roleplay/job';
+export * from '@models/roleplay/language';
+export * from '@models/roleplay/member';
+export * from '@models/roleplay/meta';
+export * from '@models/roleplay/people';
--- src/models/roleplay/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './asset';
-export * from './character';
-export * from './job';
-export * from './language';
-export * from './member';
-export * from './meta';
-export * from './people';
